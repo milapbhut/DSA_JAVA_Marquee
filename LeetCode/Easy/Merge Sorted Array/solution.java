c89b6878260bf27aea2ed2c83afa296874915e74
Bhut Milap <164072387+milapbhut@users.noreply.github.com>
2026-08-08 08:48:11 +0530

    [Syncode] LeetCode/Easy/Merge Sorted Array

---
+++ LeetCode/Easy/Merge Sorted Array/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int a = 0;
+        for(int i = 0; i < m + n; i++){
+            if(i >= m){
+                nums1[i] = nums2[a];
+                a++;
+            }
+        }
+        Arrays.sort(nums1);
+    }
+}
